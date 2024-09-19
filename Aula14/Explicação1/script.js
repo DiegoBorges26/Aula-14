89521e2fa1fceec2b61e67c704e62b9f9e17153a
DiegoBorges26 <diego.fort2011@hotmail.com>
2024-09-19 11:57:24 -0300

    Add files via upload

---
+++ Aula14/Explicação1/script.js
@@ -0,0 +1,29 @@
+
+const resultado = document.querySelector("#resultado")
+const formulario = document.querySelector("#formulario")
+const cep = document.querySelector("#cep")
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+            if (dados.erro) {
+                resultado.innerHTML = `
+                <p>CEP inválido</p>
+                `
+            } else {
+                resultado.innerHTML = `
+                <p>Endereço: ${dados.logradouro}</p>
+                <p>Complemento: ${dados.complemento}</p>
+                <p>Bairro: ${dados.bairro}</p>
+                <p>Cidade: ${dados.localidade}</p>
+                <p>Estado: ${dados.uf}</p>
+                `
+            }
+        })
+        .catch(error => resultado.innerHTML = `<p>Digite direito man</p>`)
+})
+
+
+
+
---
+++ Aula14/Explicação2/script.js
@@ -0,0 +1,30 @@
+const resultado = document.querySelector("#resultado")
+const formulario = document.querySelector("#formulario")
+const cep = document.querySelector("#cep")
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+        .then(resposta => {
+            if (!resposta.ok) {
+                throw new Error("Digite direito man")
+            }
+            return resposta.json()
+        })
+        .then(dados => {
+            if (dados.erro) {
+                resultado.innerHTML = `
+                <p>CEP inválido</p>
+                `
+            } else {
+                resultado.innerHTML = `
+                <p>Endereço: ${dados.logradouro}</p>
+                <p>Complemento: ${dados.complemento}</p>
+                <p>Bairro: ${dados.bairro}</p>
+                <p>Cidade: ${dados.localidade}</p>
+                <p>Estado: ${dados.uf}</p>
+                `
+            }
+        })
+
+        .catch(error => resultado.innerHTML = `<p>${error.message}</p>`)
+})
